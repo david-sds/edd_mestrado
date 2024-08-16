5cd9c84037a2e00475961fdba4bc0a99c71b76bd
david-sds <dave.sathler@gmail.com>
2024-08-16 16:53:55 -0300

    hashtable workign

--- aula_3/registros.py
+++ aula_3/registros.py
@@ -8,17 +8,12 @@ lista_dict = []
 
 for count in range(1, tamanho+1):
     registros = {
-        f'matricula {count}': random.randint(100000000, 999999999),
-        f'salario {count}': random.randint(1000, 9999),
-        f'código {count}': random.randint(100, 999)
+        'matricula': random.randint(100000000, 999999999),
+        'salario': random.randint(1000, 9999),
+        'código': random.randint(100, 999)
     }
     lista_dict.append(registros)
 
 for registros in lista_dict:
     print(registros)
     
-
-
-
-
-
--- aula_3/work_3.py
+++ aula_3/work_3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 def hash_str(s):
     p = 31
@@ -13,19 +14,42 @@ def hash_str(s):
     return hash_value
 
 def module(val, k):
-    key = val % k
+    key = hash_str(val) % k
     return key
 
+def init_dict(dictionary, size):
+    for _ in range(size):
+        dictionary.append([])
+        
+def search_entry(dictionary, matricula, mod):
+    key = module(matricula, mod)
+    dict_list = dictionary[key]
+    return next((x for x in dict_list if str(x['matricula']) == str(matricula)), None)
 
-if __name__ == '__main__':
+modules = [10] #[1000, 10000, 100000]
+entries = [10] #[5000, 20000, 100000]
 
-    hash_table = [[],[],[],[],[],[],[],[],[],[]]
-    val = 12349
+for mod in modules:
+    hash_table = []
+    init_dict(hash_table, mod)
+    
+    for entry_num in entries:
+        for i in range(entry_num):
+            entry = {
+                'matricula': random.randint(100000000, 999999999),
+                'salario': random.randint(1000, 9999),
+                'codigo': random.randint(100, 999)
+            }
+            val = str(entry['matricula'])
+            hash_key = module(val, mod)
+            hash_table[hash_key].append(entry)
 
-    for i in range(100):
-        val = np.random.randint(1, 10000000)
-        hash_key = module(val, 10)
-        hash_table[hash_key].append(val)
+        for i in range(len(hash_table)):
+            print(hash_table[i])
 
-
-    print(hash_table)
+    is_exit = False
+    while is_exit != True:
+        user_input = input('Search matricula (0 to exit): ')
+        if (user_input == '0'):
+            is_exit = True
+        print(search_entry(hash_table, user_input, mod))
