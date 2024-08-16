5d3022198508f3da96918839318a0d80a82a26c7
argemiro_oliveira <140517194+argewar@users.noreply.github.com>
2024-08-16 15:49:24 -0300

    olha ai david

--- aula_3/registros.py
+++ aula_3/registros.py
@@ -4,8 +4,10 @@ import random
 #matricula; salario; codigo setor
 
 tamanho = 2
+dicionario_2 = {'brasil': 'sp'}
 
 dicionario = {f'matricula {i}': random.randint(100000000, 999999999)
               for i in range(1, tamanho + 1)}
 
 print(dicionario)
+print(dicionario_2)
