d768859c7bc28bad436a74bd5f6306079a6cebef
argemiro_oliveira <140517194+argewar@users.noreply.github.com>
2024-08-16 16:04:51 -0300

    olha ai david

--- aula_3/registros.py
+++ aula_3/registros.py
@@ -8,7 +8,9 @@ lista_dict = []
 
 for count in range(1, tamanho+1):
     registros = {
-        f'matricula {count}': random.randint(100000000, 999999999)
+        f'matricula {count}': random.randint(100000000, 999999999),
+        f'salario {count}': random.randint(1000, 9999),
+        f'código {count}': random.randint(100, 999)
     }
     lista_dict.append(registros)
 
