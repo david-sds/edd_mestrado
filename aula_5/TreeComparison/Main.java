460a26e058aa32cea4e83c73f5c9833f2242d3ea
david-sds <dave.sathler@gmail.com>
2024-09-10 18:01:44 -0300

    test with different number of items

--- aula_5/TreeComparison/Main.java
+++ aula_5/TreeComparison/Main.java
@@ -12,64 +12,68 @@ public class Main {
     static Random random = new Random();
 
     public static void main(String[] args) {
-        Random random = new Random();
+        int[] seeds = {0, 1, 2, 3, 4};
+        int[] numbers = {100, 1000, 10000, 100000, 1000000};
+        for (int j = 0; j < 5; j++) {
+            Random random = new Random(seeds[j]);
+            int numberOfItems = numbers[j];
 
-        int numberOfItems = 1000000;
-        Set<Integer> set = new HashSet<Integer>();
+            Set<Integer> set = new HashSet<Integer>();
 
-        // Generate random numbers until we have 'n' unique numbers
-        while (set.size() < numberOfItems) {
-            int randomNumber = random.nextInt(numberOfItems * 1000);
-            set.add(randomNumber);
-        }
-        
-        List<Integer> list = new ArrayList<>(set);
-
-        AvlTree
-         avlTree = new AvlTree();
-        RedBlackTree<Integer> redBlackTree = new RedBlackTree<Integer>();
-
-        for (Integer num : list) {
-            avlTree.insertData(num);
-            redBlackTree.insert(num);
-        }
-
-        int numberOfSearchs = 10000;
-        int[] searchValues = new int[numberOfSearchs];
-        long[] elepsedTimesRBT = new long[numberOfSearchs];
-        long[] elepsedTimesBT = new long[numberOfSearchs];
-        
-        for (int i = 0; i < searchValues.length; i++) {
-            int randomIndex = random.nextInt(list.size());
-            searchValues[i] = list.get(randomIndex);
-        }
-
-        for (int i = 0; i < searchValues.length; i++) {
-            int value = searchValues[i];
-            long start, end, elapsed;
+            // Generate random numbers until we have 'n' unique numbers
+            while (set.size() < numberOfItems) {
+                int randomNumber = random.nextInt(numberOfItems * 1000);
+                set.add(randomNumber);
+            }
             
-            start = System.nanoTime();
-            avlTree.searchData(value);
-            end = System.nanoTime();
-            elapsed = end - start;
-            elepsedTimesBT[i] = elapsed;
-            
-            start = System.nanoTime();
-            redBlackTree.search(value);
-            end = System.nanoTime();
-            elapsed = end - start;
-            elepsedTimesRBT[i] = elapsed;
+            List<Integer> list = new ArrayList<>(set);
 
-            System.out.println(
-                "Search " + (i + 1) + ": " + value + "; " +
-                "(BT, RBT) => (" + elepsedTimesBT[i] + "ns, " + elepsedTimesRBT[i] + "ns)."
-            );
+            AvlTree
+            avlTree = new AvlTree();
+            RedBlackTree<Integer> redBlackTree = new RedBlackTree<Integer>();
+
+            for (Integer num : list) {
+                avlTree.insertData(num);
+                redBlackTree.insert(num);
+            }
+
+            int numberOfSearchs = 10000;
+            int[] searchValues = new int[numberOfSearchs];
+            long[] elepsedTimesRBT = new long[numberOfSearchs];
+            long[] elepsedTimesBT = new long[numberOfSearchs];
+            
+            for (int i = 0; i < searchValues.length; i++) {
+                int randomIndex = random.nextInt(list.size());
+                searchValues[i] = list.get(randomIndex);
+            }
+
+            for (int i = 0; i < searchValues.length; i++) {
+                int value = searchValues[i];
+                long start, end, elapsed;
+                
+                start = System.nanoTime();
+                avlTree.searchData(value);
+                end = System.nanoTime();
+                elapsed = end - start;
+                elepsedTimesBT[i] = elapsed;
+                
+                start = System.nanoTime();
+                redBlackTree.search(value);
+                end = System.nanoTime();
+                elapsed = end - start;
+                elepsedTimesRBT[i] = elapsed;
+
+                // System.out.println(
+                //     "Search " + (i + 1) + ": " + value + "; " +
+                //     "(BT, RBT) => (" + elepsedTimesBT[i] + "ns, " + elepsedTimesRBT[i] + "ns)."
+                // );
+            }
+            
+            System.out.println("\nNumber of items: " + numberOfItems);
+            System.out.println("Average search times:");
+            System.out.println("> Red Black Tree: " + LongStream.of(elepsedTimesRBT).average().getAsDouble() + "ns.");
+            System.out.println("> Binary Tree: " + LongStream.of(elepsedTimesBT).average().getAsDouble() + "ns.");
         }
-        
-        System.out.println();
-        System.out.println("Average search times:");
-        System.out.println("> Red Black Tree: " + LongStream.of(elepsedTimesRBT).average().getAsDouble() + "ns.");
-        System.out.println("> Binary Tree: " + LongStream.of(elepsedTimesBT).average().getAsDouble() + "ns.");
     }
 
 }
